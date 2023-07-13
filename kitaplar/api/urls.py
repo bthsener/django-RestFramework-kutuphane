0016592e91ab5710b6396e448a8595975b765b4f
abatuhan06 <abatuhan0666@gmail.com>
2023-07-13 10:31:34 +0300

    added genericAPIView

--- kitaplar/api/urls.py
+++ kitaplar/api/urls.py
@@ -3,4 +3,5 @@ from kitaplar.api import views as api_view
 
 urlpatterns = [
     # path('kitaplar/<int:pk>', api_view.KitaplarDetailsAPIView.as_view(), name='kitap-detail'),
+    path('kitaplar/', api_view.KitaplarListCreateAPIView.as_view(), name='kitap-list')
 ]
--- kitaplar/api/views.py
+++ kitaplar/api/views.py
@@ -1,7 +1,18 @@
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import *
 from rest_framework.views import APIView
 from kitaplar.models import Kitap, Yorum
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 
+class KitaplarListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+    queryset = Kitap.objects.all()
+    serializer_class = KitapSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
